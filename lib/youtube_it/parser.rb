7e7055a59dfa1e7d59fac8252098f3661325959d
Darren Fung <fubrub@gmail.com>
2012-03-22 22:43:20 -0300

    Fix access to accessControl permission

--- lib/youtube_it/parser.rb
+++ lib/youtube_it/parser.rb
@@ -374,9 +374,9 @@ class YouTubeIt
         access_control.each do |access|
           case access.attributes["action"]
             when "embed"
-              noembed = access.attributes.permission == 'allowed' ? false : true
+              noembed = access.attributes["permission"] == 'allowed' ? false : true
             when "list"
-              listed = access.attributes.permission == 'allowed' ? true : false
+              listed = access.attributes["permission"] == 'allowed' ? true : false
             else
               # Do nothing
           end
